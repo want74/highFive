24a70b31a517008b0339e3ee1b171c1f10a515ef
want74 <want74@rambler.ru>
2022-05-05 16:31:13 +0900

    start

---
+++ index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <title>High five</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row panel" style="height: 100vh;">
+            <div class="col-10 mx-auto" style="height: 100vh;">
+                <div class="row" style="height: 100vh;">
+                    <div class="col my-auto">
+                        <div class="row">
+                            <h1 class="project text-white">
+                                High five
+                            </h1>
+                        </div>
+                        <div class="row">
+                            <p class="slogan">
+                                Найди своих
+                                единомышленников
+                                и развивайся !
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row panel" style="height: 100vh;">
+            ss
+        </div>
+    </div>
+
+    <script src="script/jquery-2.2.1.min.js"></script>
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
+    </script>
+    <script src="script/jquery.scrollify.js"></script>
+    <script>
+        $.scrollify({
+            section: ".panel",
+            scrollbars: false,
+            scrollSpeed: 100,
+        })
+    </script>
+</body>
+
+</html>
